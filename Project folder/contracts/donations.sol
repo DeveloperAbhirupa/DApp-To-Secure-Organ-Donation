c3e9121cd04abc5d77c09bdc67edbf77b9f5d948
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2020-02-23 03:10:36 +0530

    Event for donor creation

--- Project folder/contracts/donations.sol
+++ Project folder/contracts/donations.sol
@@ -3,15 +3,10 @@ pragma solidity 0.5.16;
 contract donations {
   //Read/write donor
 
-
-  //Constructor
-  constructor () public{
-    donor="Donor 1";
-
-  }
+  event createDonor(string name, uint did, string msg);
 
   struct donor{
-    string public donor;//Donor name;
+    string donor;//Donor name;
     string donor_id; // random generated hashcode (String unique id)
     string organ_id;//random generated hashcode (String unique id)
     string organ; //Type of organ
@@ -20,10 +15,30 @@ contract donations {
     uint hosp_id;  //random generated hashcode (String unique id)
   }
 
-  function init_donor(string memory _donor, string memory _organ, string memory _bg, uint status, uint hosp_id ) public
+  donor[] public donorArray;
+
+  /**In Solidity, functions are public by default. This means anyone (or any other contract) can call your contract's function and execute its code.
+  Obviously this isn't always desirable, and can make your contract vulnerable to attacks.
+  Thus it's good practice to mark your functions as private by default, and then only make public the
+  functions you want to expose to the world.**/
+
+  function _initDonor(string memory _donor, string memory _organ, string memory _bg, uint _status, uint _hosp_id ) private
   {
     //Note: Memory keyword denotes call by value for String
+    string did = _generateRandomID(_donor);
+    string oid = _generateRandomID(_organ);
+    uint length = donorArray.push(donor(_donor, did, oid,_organ,_bg,_status,_hosp_id))- 1;
+    string msg = "Donor has been succesfully created.";
+    emit createDonor(_donor, did, msg);
+
   }
+
+  //keccak256 isnt a totally secure approach, anything else? Think later.
+
+  function _generateRandomID(string memory _str) private view returns (uint) {
+      uint rand = uint(keccak256(abi.encodePacked(_str)));
+      return rand % 10;
+   }
 
 
   //Module wise division:
